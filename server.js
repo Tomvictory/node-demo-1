35cf040718db0e30048131e3662bccfdf8ee440e
waiting <539959237@qq.com>
2020-01-05 21:35:34 +0800

    静态服务器node第二次实验

--- server.js
+++ server.js
@@ -21,23 +21,28 @@ var server = http.createServer(function(request, response) {
 
     /******** 从这里开始看，上面不要看 ************/
 
-    if (path === "/") {
-        response.statusCode = 200;
-        response.setHeader("Content-Type", "text/html;charset=utf-8");
-        response.write(`
-          <link rel="stylesheet" href="/style.css">
-          <h1>你好</h1>
-        `);
-        response.end();
-    } else if (path === "/style.css") {
-        response.statusCode = 200;
-        response.setHeader("Content-Type", "text/css;charset=utf-8");
-        response.write(`h1{color: red;}`);
-        response.end();
-    } else {
-        response.statusCode = 404;
-        response.end();
+    console.log('有个傻子发请求过来啦！路径（带查询参数）为：' + pathWithQuery)
+    response.statusCode = 200;
+
+    const filePath = path === '/' ? '/index.html' : path
+    const index = filePath.lastIndexOf('.')
+    const suffix = filePath.substring(index)
+    const fileTypes = {
+        '.html': 'text/html',
+        '.css': 'text/css',
+        '.js': 'text/javascript'
     }
+    response.setHeader('Content-Type', `${fileTypes[suffix] || 'text/html'};charset=utf-8`);
+    let content
+    try {
+        content = fs.readFileSync(`./public${filePath}`)
+    } catch {
+        content = '文本不存在'
+        response.statusCode = 404
+    }
+    response.write(content);
+    response.end();
+
 
     /******** 代码结束，下面不要看 ************/
 });
